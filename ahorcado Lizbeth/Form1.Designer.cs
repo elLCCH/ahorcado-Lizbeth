a8105304b9f2352d0a1a907044fecb7df344ab37
Luis Choque <cristian.98oruro@gmail.com>
2021-09-20 01:07:21 -0400

    TODO CODIGO INTERNO

--- ahorcado Lizbeth/Form1.Designer.cs
+++ ahorcado Lizbeth/Form1.Designer.cs
@@ -31,34 +31,121 @@ namespace ahorcado_Lizbeth
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pCabeza = new System.Windows.Forms.PictureBox();
+            this.pBrazos = new System.Windows.Forms.PictureBox();
+            this.pEstomago = new System.Windows.Forms.PictureBox();
+            this.pPiernas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pPalo2 = new System.Windows.Forms.PictureBox();
+            this.pPalo1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pCabeza)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBrazos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pEstomago)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPiernas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPalo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPalo1)).BeginInit();
             this.SuspendLayout();
             // 
             // pCabeza
             // 
             this.pCabeza.Image = ((System.Drawing.Image)(resources.GetObject("pCabeza.Image")));
-            this.pCabeza.Location = new System.Drawing.Point(140, 37);
+            this.pCabeza.Location = new System.Drawing.Point(226, 124);
             this.pCabeza.Name = "pCabeza";
             this.pCabeza.Size = new System.Drawing.Size(90, 79);
             this.pCabeza.TabIndex = 0;
             this.pCabeza.TabStop = false;
             // 
+            // pBrazos
+            // 
+            this.pBrazos.Image = ((System.Drawing.Image)(resources.GetObject("pBrazos.Image")));
+            this.pBrazos.Location = new System.Drawing.Point(223, 199);
+            this.pBrazos.Name = "pBrazos";
+            this.pBrazos.Size = new System.Drawing.Size(90, 79);
+            this.pBrazos.TabIndex = 1;
+            this.pBrazos.TabStop = false;
+            // 
+            // pEstomago
+            // 
+            this.pEstomago.Image = ((System.Drawing.Image)(resources.GetObject("pEstomago.Image")));
+            this.pEstomago.Location = new System.Drawing.Point(257, 225);
+            this.pEstomago.Name = "pEstomago";
+            this.pEstomago.Size = new System.Drawing.Size(42, 79);
+            this.pEstomago.TabIndex = 2;
+            this.pEstomago.TabStop = false;
+            // 
+            // pPiernas
+            // 
+            this.pPiernas.Image = ((System.Drawing.Image)(resources.GetObject("pPiernas.Image")));
+            this.pPiernas.Location = new System.Drawing.Point(251, 251);
+            this.pPiernas.Name = "pPiernas";
+            this.pPiernas.Size = new System.Drawing.Size(90, 79);
+            this.pPiernas.TabIndex = 3;
+            this.pPiernas.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(344, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 86);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // pPalo2
+            // 
+            this.pPalo2.Image = ((System.Drawing.Image)(resources.GetObject("pPalo2.Image")));
+            this.pPalo2.Location = new System.Drawing.Point(116, 52);
+            this.pPalo2.Name = "pPalo2";
+            this.pPalo2.Size = new System.Drawing.Size(216, 79);
+            this.pPalo2.TabIndex = 6;
+            this.pPalo2.TabStop = false;
+            // 
+            // pPalo1
+            // 
+            this.pPalo1.Image = ((System.Drawing.Image)(resources.GetObject("pPalo1.Image")));
+            this.pPalo1.Location = new System.Drawing.Point(63, 52);
+            this.pPalo1.Name = "pPalo1";
+            this.pPalo1.Size = new System.Drawing.Size(90, 315);
+            this.pPalo1.TabIndex = 7;
+            this.pPalo1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(544, 411);
+            this.ClientSize = new System.Drawing.Size(642, 497);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pPiernas);
+            this.Controls.Add(this.pEstomago);
+            this.Controls.Add(this.pBrazos);
             this.Controls.Add(this.pCabeza);
+            this.Controls.Add(this.pPalo2);
+            this.Controls.Add(this.pPalo1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pCabeza)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBrazos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pEstomago)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPiernas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPalo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPalo1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pCabeza;
+        private System.Windows.Forms.PictureBox pBrazos;
+        private System.Windows.Forms.PictureBox pEstomago;
+        private System.Windows.Forms.PictureBox pPiernas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pPalo2;
+        private System.Windows.Forms.PictureBox pPalo1;
     }
 }
 
--- ahorcado Lizbeth/Form1.cs
+++ ahorcado Lizbeth/Form1.cs
@@ -12,10 +12,146 @@ namespace ahorcado_Lizbeth
 {
     public partial class Form1 : Form
     {
+        string Guardacion;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ////Para obligar a que sólo se introduzcan números
+            //if (Char.IsDigit(e.KeyChar))
+            //{
+            //    e.Handled = false;
+            //}
+            //else
+            //  if (Char.IsControl(e.KeyChar)) //permitir teclas de control como retroceso
+            //{
+            //    e.Handled = false;
+            //}
+            //else
+            //{
+            //    //el resto de teclas pulsadas se desactivan
+            //    e.Handled = true;
+            //}
+            
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            string letra = Convert.ToString(e.KeyData);
+
+            MessageBox.Show("presionaste: "+letra);
+
+            CompararLetra(letra);
+            //if (e.KeyValue == 13)
+            //{
+            //    MessageBox.Show("presionaste enter");
+            //}
+        }
+
+        private void CompararLetra(string pLetra)
+        {
+            string PalabraClave = "GABRIELA", Palabra="";
+            bool x = PalabraClave.Contains(pLetra);
+            string[] resultado = new string[PalabraClave.Length];
+            string barritas = "";
+            string nuevaPalabra="";
+            for (int i = 0; i < PalabraClave.Length; i++)
+            {
+                barritas = barritas + "_";
+
+            }
+            label1.Text = barritas;
+            //MessageBox.Show("stop");
+            if (x==true)
+            {
+                
+                for (int i = 0; i < PalabraClave.Length; i++)
+                {
+                    int locacion = (PalabraClave.IndexOf(pLetra, i));
+                    //MessageBox.Show(Convert.ToString(locacion));
+
+                    //if (locacion==null)
+                    //{
+                    //    resultado[i] = "_";
+                    //}
+                    //else
+                    //{
+                    //    resultado[i] = pLetra;
+                    //}
+
+                    if (i == locacion) 
+                        Palabra = Palabra + pLetra;
+                   
+                    else
+                        Palabra = Palabra + "_";
+                    //resultado[locacion] = pLetra;
+                }
+
+                //MessageBox.Show(Palabra);
+                label1.Text = Palabra;
+                if (Guardacion!=null)
+                {
+                    //comparar guardacion y Palabra
+                    //Palabra->    _R_R_
+                    //Guardacion-> O___O
+
+                    char[] Palabra1 = Palabra.ToCharArray();
+                    char[] Guardacion1 = Guardacion.ToCharArray();
+                    //string[] Palabra1 = new string[] { Palabra };
+                    //string[] Guardacion1 = new string[] { Guardacion };
+
+
+                    for (int i = 0; i < Palabra.Length; i++)
+                    {
+                        //MessageBox.Show("valor de la i:"+Convert.ToString(i));
+                        //for (int i = 0; i < length; i++)
+                        //{
+
+                        //}
+                        if (Convert.ToString(Guardacion1[i])=="_")
+                        {
+                            //MessageBox.Show("valor del vector Guardacion1:" + Convert.ToString(Guardacion1[i]));
+                            //MessageBox.Show("valor del vector Palabra1:" + Convert.ToString(Palabra1[i]));
+                            Guardacion1[i] = Palabra1[i];
+                        }
+                        
+                    }
+                    nuevaPalabra = String.Concat(Guardacion1);
+                   Guardacion = nuevaPalabra;
+                    //MessageBox.Show("nueva:" + nuevaPalabra);
+                    label1.Text = nuevaPalabra;
+                }
+                else
+                {
+                    Guardacion = Palabra;
+                }
+
+                
+            }
+            else
+            {
+                //CompararLetra(Palabra);
+                MessageBox.Show("No encontrado");
+                label1.Text = Guardacion;
+            }
+            
+            //for (int i = 0; i < resultado.Length; i++)
+            //{
+                
+                
+            //}
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
